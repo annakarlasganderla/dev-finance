ddccf05b40ae09914d1058b32187f4eaf4ba3b9f
Anna sganderla <sganderlakarlaanna@gmail.com>
2021-05-18 17:25:22 -0300

    adicionando o modal e JavaScript

---
+++ main.js
@@ -0,0 +1,15 @@
+const modal = {
+    open() {
+      // abrir modal
+      // adicionar classe active ao modal
+      document.querySelector('.modal-overlay')   // pesquisa pelo seletor
+      .classList.add('active')  // adiciona active
+    },
+    close() {
+      // fechar o MODAL
+      // remover a classe active do modal
+      document.querySelector('.modal-overlay')   // pesquisa pelo seletor
+      .classList.remove('active')  // remove o active
+    }
+  }
+  
